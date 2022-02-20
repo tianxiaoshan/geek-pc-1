09fa292447980551c23835eaf7bb41b867156039
shanshan <994606798@qq.com>
2022-02-20 16:38:59 +0800

    登录功能-表单校验

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Layout'
 import Login from './pages/Login'
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,90 @@
 import React from 'react'
-import { Card } from 'antd'
-import logo from '../../assets/images/logo.png'
-import './index.css'
+import { Card, Form, Input, Button, Checkbox } from 'antd'
+import logo from 'assets/images/logo.png'
+import './index.scss'
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values)
+  }
   return (
     <div className="login">
       <Card className="login-enter">
         <img src={logo} alt="" className="login-logo" />
+        {/* 表单 */}
+        <Form
+          autoComplete="off"
+          size="large"
+          validateTrigger={['onChange', 'onBlur']}
+          onFinish={onFinish}
+          initialValues={{
+            mobile: '13911111111',
+            code: '246810',
+            agree: 'true',
+          }}
+        >
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: '手机号不能为空',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号格式错误',
+              },
+            ]}
+          >
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '验证码不能为空',
+              },
+              {
+                pattern: /^\d{6}$/,
+                message: '验证码格式错误',
+              },
+            ]}
+          >
+            <Input placeholder="请输入验证码" />
+          </Form.Item>
+
+          <Form.Item
+            name="agree"
+            rules={[
+              {
+                // 自定义校验规则
+                validator(rule, value) {
+                  if (value) {
+                    // 自定义校验规则 通过这边可以发请求 去看用户名是否重复  重复了就走reject
+                    return Promise.resolve()
+                  } else {
+                    return Promise.reject(new Error('请阅读并同意用户协议'))
+                  }
+                },
+              },
+            ]}
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 0.5,
+              span: 20,
+            }}
+          >
+            <Checkbox>我已经同意[隐私条款]和[用户协议]</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )

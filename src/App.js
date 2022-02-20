eded12bd3eda58219d54ea4821d50ab8a5de5ec4
shanshan <994606798@qq.com>
2022-02-20 13:50:02 +0800

    项目搭建-路由配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
+
 function App() {
-  return <div className="App">App根组件</div>
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link>
+
+        {/* 配置路由的规则 */}
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Layout = () => {
+  return <div>首页布局</div>
+}
+export default Layout
